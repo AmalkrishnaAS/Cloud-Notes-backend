3b53947a7b4fcb2534b9fb4533c33594fff33b92
amal krishna <amalkrishna02016@gmail.com>
2021-09-11 12:51:50 +0530

    createuser

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,15 @@ const router = express.Router()
 const {Schema}=mongoose
 const User=require('../models/User');
 const { response } = require('express');
+const bcrypt=require('bcryptjs')
+const jwt=require('jsonwebtoken')
+
+const JWT_SECRET='amalisagood$boy'
+
+
+
 //create a user using: POST "/api/auth/createuser".doesnt require login
+
 router.post('/createuser',[
     body('email','Enter a valid email').isEmail(),
     body('name','Enter a valid name').isLength({min:3}),
@@ -20,21 +28,45 @@ router.post('/createuser',[
     
     
   }
+  try
+  {
   let user=await User.findOne({email:req.body.email})
   if(user)
   {
     return res.status(400).json({error:"Email already exists"})
   }
+
+
+
+  //generate hash
+
+
+  const salt=await bcrypt.genSalt(10)
+  secPass=await bcrypt.hash(req.body.password,salt)
+
+
+
+  //create user
   user=await User.create({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password
+    password: secPass
   })
 //   .then(user => res.json(user))
 //   .catch(err=>{console.log(err);
 // res.json({error:"Email already exists",message:err.message})})
-res.json(user)
+const data={
+  user:{
+    id:user.id
+  }
+}
+const authtoken=jwt.sign(data,JWT_SECRET)
+console.log(authtoken)
+res.json({authtoken:authtoken})
   
-
+}catch(error){
+  console.error(error.message);
+  res.status(500).send("Something went wrong")
+}
 })
 module.exports =router;
