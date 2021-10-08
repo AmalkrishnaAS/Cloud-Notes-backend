d2b4eaffdcc6617de96f31b31e2ed78e87abb0c7
amal krishna <amalkrishna02016@gmail.com>
2021-10-08 22:44:14 +0530

    cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const connectToMongo=require('./db')
 const express = require('express')
+var cors = require('cors')
 const app = express()
 const port = 5000
 connectToMongo();
+app.use(cors())
 app.use(express.json())
 //available routes
 
